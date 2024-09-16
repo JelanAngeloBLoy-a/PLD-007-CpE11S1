e0132f83715d3071d730a35fc8ed0551d5fef05e
JelanAngeloBLoy-a <qjaloy-a01@tip.edu.ph>
2024-09-16 12:01:05 +0800

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int i;
+    int j = 10;
+    
+    for(i = 0; i <= j; i++)
+    {
+        cout << "Hello " << i << endl; 
+    }
+    
+    return 0;
+}
