70515be3fb02e50418f4a6a8119f325cb89e2ccd
JelanAngeloBLoy-a <qjaloy-a01@tip.edu.ph>
2024-11-25 12:04:02 +0800

    Create Exercise 12.cpp

---
+++ Finals/Exercise 12.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+	
+string str="123";
+int num=atoi(str.c_str()); //Convert string to integer
+cout<<"Converted number:"<<num<<endl;
+
+	
+return 0;
+	
+	
+}
