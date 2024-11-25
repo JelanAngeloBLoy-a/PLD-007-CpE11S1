3558596ab448f92e0113931168c32cf323223fe9
JelanAngeloBLoy-a <qjaloy-a01@tip.edu.ph>
2024-11-25 13:23:54 +0800

    Create Handson_Activity_6.1_2.1_Loy-a.cpp

---
+++ Finals/Handson_Activity_6.1_2.1_Loy-a.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include <cmath>
+#include <cstdlib>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int num1, num2, sum, choice, choose;
+	bool constant= true;
+	cout<<"Hello to you, we are to calculate number for you"<<endl;
+	cout<<"\n Input number\n";
+	cin>>num1;
+	
+	cout<<"\n Input 2nd number\n";
+	cin>>num2;
+	
+	cout<<"\nPress 1 to add numbers"<<endl;
+	cout<<"Press 2 to subtract"<<endl;
+	cout<<"Press 3 to multiply"<<endl;
+	cout<<"Press 4 to divide"<<endl;
+	cin>> choice;
+	
+	while(constant)
+	{
+	
+	switch (choice)
+	{
+	 case 1:
+	 	sum=fma(1.0, num1, num2);
+	 	cout<<"\nthe sum of 2 added numbers is: "<<sum<<endl;
+	 	
+		cout<<"\n Input number\n";
+	cin>>num1;
+	
+	cout<<"\n Input 2nd number\n";
+	cin>>num2;
+	
+	cout<<"\nPress 1 to add numbers"<<endl;
+	cout<<"Press 2 to subtract"<<endl;
+	cout<<"Press 3 to multiply"<<endl;
+	cout<<"Press 4 to divide"<<endl;
+	cin>> choice;
+	 break;	
+		
+	case 2:
+		sum=fma(-1.0, num2, num1);
+		cout<<"\nthe sum of 2 subtracted numbers is: "<<sum<<endl;
+	 	cout<<"\nInput choose to whether you like to add, subtract, multiply or divide"<<endl;
+	 	
+	cout<<"\n Input number\n";
+	cin>>num1;
+	
+	cout<<"\n Input 2nd number\n";
+	cin>>num2;
+	
+	cout<<"\nPress 1 to add numbers"<<endl;
+	cout<<"Press 2 to subtract"<<endl;
+	cout<<"Press 3 to multiply"<<endl;
+	cout<<"Press 4 to divide"<<endl;
+	cin>> choice;
+	 break;	
+	 
+	 case 3:
+	 sum=fma(num1, num2, 0.0);
+	 cout<<"\nthe sum of 2 multiplied numbers is: "<<sum<<endl;
+	 
+	 	cout<<"\nInput choose to whether you like to add, subtract, multiply or divide"<<endl;	
+	
+	 	
+	cout<<"\n Input number\n";
+	cin>>num1;
+	
+	cout<<"\n Input 2nd number\n";
+	cin>>num2;
+	
+	cout<<"\nPress 1 to add numbers"<<endl;
+	cout<<"Press 2 to subtract"<<endl;
+	cout<<"Press 3 to multiply"<<endl;
+	cout<<"Press 4 to divide"<<endl;
+	cin>> choice;
+	 break;	
+	 
+	 case 4:
+	 div_t result = div(num1, num2);
+    cout << "\nQuotient: " << result.quot << std::endl;
+    cout << "Remainder: " << result.rem << std::endl;	
+    
+   	cout<<"\n Input number\n";
+	cin>>num1;
+	
+	cout<<"\n Input 2nd number\n";
+	cin>>num2;
+	
+	cout<<"\nPress 1 to add numbers"<<endl;
+	cout<<"Press 2 to subtract"<<endl;
+	cout<<"Press 3 to multiply"<<endl;
+	cout<<"Press 4 to divide"<<endl;
+	cin>> choice; 	
+	 break;	
+   }
+		
+		
+	}
+	return 0;
+}
